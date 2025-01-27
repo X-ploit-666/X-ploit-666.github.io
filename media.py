8c473664ca83ff56a03c63b4ef32e2d0728b6100
X-ploit-666 <103617891+X-ploit-666@users.noreply.github.com>
2025-01-27 05:55:59 +0000

    Pending changes exported from your codespace

---
+++ media.py
@@ -0,0 +1,33 @@
+import os
+import re
+
+def replace_footer_content(file_path):
+    # Read the content of the file
+    with open(file_path, 'r', encoding='utf-8') as file:
+        content = file.read()
+
+    vvar="""onclick="this.href='whatsapp://send?&phone=+4915750178884&text=%0A- Quantity: ' + document.getElementsByClassName('quantity__input')[0].value + '%0A- Link: https://panel-pix.com' + encodeURIComponent(window.location.pathname);"""
+
+    # Regex pattern to find <div> elements with class 'footer-block__details-content rte'
+    pattern = r'''href=\"https:\/\/wa.me\/8613378643944'''
+    # Replace matched content with empty string
+    modified_content = re.sub(pattern, vvar, content, flags=re.DOTALL)
+
+    # Write the modified content back to the file
+    with open(file_path, 'w', encoding='utf-8') as file:
+        file.write(modified_content)
+
+def process_directory(directory):
+    # Walk through the directory recursively
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith('.html') or file.endswith('.htm'):  # Adjust file types as needed
+                file_path = os.path.join(root, file)
+                print(f'Processing file: {file_path}')
+                replace_footer_content(file_path)
+
+if __name__ == '__main__':
+    target_directory = input("Enter the directory path to process: ")
+    process_directory(target_directory)
+    print("Processing complete.")
+#href="https://wa.me/8613378643944"
